41b4261ce9058bbb1ced8b186aa85499d9d20812
NICK VALLEE <nickvallee@gmail.com>
2016-12-13 11:24:41 -0500

    Completed lazyloading of images

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -11207,6 +11207,7 @@
 	    function StickyHeader() {
 	        _classCallCheck(this, StickyHeader);
 
+	        this.lazyImages = (0, _jquery2.default)(".lazyload");
 	        this.siteHeader = (0, _jquery2.default)(".site-header");
 	        this.headerTriggerElement = (0, _jquery2.default)(".large-hero__title");
 	        this.createHeaderWaypoint();
@@ -11214,9 +11215,17 @@
 	        this.headerLinks = (0, _jquery2.default)(".primary-nav a");
 	        this.createPageSectionWaypoints();
 	        this.addSmoothScrolling();
+	        this.refreshWayPoints();
 	    }
 
 	    _createClass(StickyHeader, [{
+	        key: 'refreshWayPoints',
+	        value: function refreshWayPoints() {
+	            this.lazyImages.load(function () {
+	                Waypoint.refreshAll();
+	            });
+	        }
+	    }, {
 	        key: 'addSmoothScrolling',
 	        value: function addSmoothScrolling() {
 	            this.headerLinks.smoothScroll();
